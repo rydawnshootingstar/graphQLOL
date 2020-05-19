89f45d9af0ea10380aba599be95d0a4cfa14a684
Ryan <rydawnshootingstar@gmail.com>
2020-05-19 14:22:34 -0400

    graphql project directory structure pt 2

--- exercises/graphql-basics/src/resolvers/Mutation.js
+++ exercises/graphql-basics/src/resolvers/Mutation.js
@@ -1,132 +1,162 @@
 import { v4 as uuid } from "uuid";
 const Mutation = {
-        createUser(parent, args, {db}, info) {
+    createUser(parent, args, { db }, info) {
+        const emailTaken = db.users.some((user) => {
+            return user.email === args.data.email;
+        });
+
+        if (emailTaken) {
+            throw new Error(
+                `A user with this email already exists. Please choose a unique email.`
+            );
+        }
+
+        const user = {
+            id: uuid(),
+            ...args.data,
+        };
+
+        db.users.push(user);
+        return user;
+    },
+    updateUser(parent, { id, data }, { db }, info) {
+        const user = db.users.find((user) => {
+            return user.id === id;
+        });
+
+        if (!user) {
+            throw new Error(`This id does not belong to a user`);
+        }
+
+        if (typeof data.email === "string") {
             const emailTaken = db.users.some((user) => {
-                return user.email === args.data.email;
+                return user.email === data.email;
             });
 
-            if (emailTaken) {
+            if (emailTaken && data.email !== user.email) {
                 throw new Error(
-                    `A user with this email already exists. Please choose a unique email.`
+                    `A user with this email already exists. Please choose a unique email`
                 );
             }
 
-            const user = {
-                id: uuid(),
-                ...args.data,
-            };
+            user.email = data.email;
+        }
+        if (typeof data.name === "string") {
+            user.name = data.name;
+        }
 
-            db.users.push(user);
-            return user;
-        },
-        deleteUser(parent, args, {db}, info) {
-            const userIndex = db.users.findIndex((user) => {
-                return user.id === args.id;
-            });
+        if (typeof data.age !== "undefined") {
+            user.age = data.age;
+        }
+        return user;
+    },
+    deleteUser(parent, args, { db }, info) {
+        const userIndex = db.users.findIndex((user) => {
+            return user.id === args.id;
+        });
 
-            if (userIndex === -1) {
-                throw new Error(`This id does not belong to a user`);
+        if (userIndex === -1) {
+            throw new Error(`This id does not belong to a user`);
+        }
+
+        // remember, splice returns values that were removed, and we want them in this case
+        // this will delete the user
+        const deletedUsers = db.users.splice(userIndex, 1);
+
+        // delete all of a user's posts, and each of those post's comments
+        db.proasts = db.proasts.filter((proast) => {
+            const postsToDelete = proast.author === args.id;
+
+            if (postsToDelete) {
+                db.comments = db.comments.filter((comment) => {
+                    return comment.post !== proast.id;
+                });
             }
 
-            // remember, splice returns values that were removed, and we want them in this case
-            // this will delete the user
-            const deletedUsers = db.users.splice(userIndex, 1);
+            return !postsToDelete;
+        });
 
-            // delete all of a user's posts, and each of those post's comments
-            db.proasts = db.proasts.filter((proast) => {
-                const postsToDelete = proast.author === args.id;
+        // delete all of a user's comments
+        db.comments = db.comments.filter((comment) => {
+            return comment.author !== args.id;
+        });
 
-                if (postsToDelete) {
-                    db.comments = db.comments.filter((comment) => {
-                        return comment.post !== proast.id;
-                    });
-                }
+        return deletedUsers[0];
+    },
+    createPost(parent, args, { db }, info) {
+        const userExists = db.users.some((user) => {
+            return user.id === args.data.author;
+        });
 
-                return !postsToDelete;
-            });
+        if (!userExists) {
+            throw new Error(`This id does not belong to a user.`);
+        }
 
-            // delete all of a user's comments
-            db.comments = db.comments.filter((comment) => {
-                return comment.author !== args.id;
-            });
+        const post = {
+            id: uuid(),
+            ...args.data,
+        };
 
-            return deletedUsers[0];
-        },
-        createPost(parent, args, {db}, info) {
-            const userExists = db.users.some((user) => {
-                return user.id === args.data.author;
-            });
+        db.proasts.push(post);
+        return post;
+    },
+    deletePost(parent, args, { db }, info) {
+        const postToDelete = db.proasts.findIndex((post) => {
+            return post.id === args.id;
+        });
 
-            if (!userExists) {
-                throw new Error(`This id does not belong to a user.`);
-            }
+        if (postToDelete === -1) {
+            throw new Error(`No post with this id exists`);
+        }
 
-            const post = {
-                id: uuid(),
-                ...args.data,
-            };
+        // deletes posts and store returned value in array
+        const deletedProasts = db.proasts.splice(postToDelete, 1);
 
-            db.proasts.push(post);
-            return post;
-        },
-        deletePost(parent, args, {db}, info){
-            const postToDelete = db.proasts.findIndex((post)=> {
-                return post.id === args.id;
-            });
+        // delete all comments associated with post
+        db.comments.filter((comment) => {
+            return comment.post !== args.id;
+        });
 
-            if(postToDelete === -1){
-                throw new Error(`No post with this id exists`);
-            }
+        return deletedProasts[0];
+    },
+    createComment(parent, args, { db }, info) {
+        const userExists = db.users.some((user) => {
+            return user.id === args.data.author;
+        });
 
-            // deletes posts and store returned value in array
-           const deletedProasts = db.proasts.splice(postToDelete, 1);
+        const postExists = db.proasts.filter((proast) => {
+            return proast.id === args.data.post && proast.published;
+        });
 
-           // delete all comments associated with post
-           db.comments.filter((comment)=> {
-               return comment.post !== args.id
-           });
+        if (!userExists) {
+            throw new Error(`This ID does not belong to a user.`);
+        }
+        if (!postExists) {
+            throw new Error(`A published post with this ID doesn't exist`);
+        }
 
-           return deletedProasts[0];
-        
-        },
-        createComment(parent, args, {db}, info) {
-            const userExists = db.users.some((user) => {
-                return user.id === args.data.author;
-            });
+        const comment = {
+            id: uuid(),
+            ...args.data,
+        };
 
-            const postExists = db.proasts.filter((proast) => {
-                return proast.id === args.data.post && proast.published;
-            });
+        db.comments.push(comment);
 
-            if (!userExists) {
-                throw new Error(`This ID does not belong to a user.`);
-            }
-            if (!postExists) {
-                throw new Error(`A published post with this ID doesn't exist`);
-            }
+        return comment;
+    },
+    deleteComment(parent, args, { db }, info) {
+        const commentExists = db.comments.findIndex((comment) => {
+            return comment.id === args.id;
+        });
 
-            const comment = {
-                id: uuid(),
-                ...args.data,
-            };
+        if (commentExists === -1) {
+            throw new Error(`No comment with this id exists`);
+        }
 
-            db.comments.push(comment);
+        const deletedComments = db.comments.splice(commentExists, 1);
 
-            return comment;
-        },
-        deleteComment(parent, args, {db}, info){
-            const commentExists = db.comments.findIndex((comment)=> {
-                return comment.id === args.id;
-            });
+        return deletedComments[0];
+    },
+};
 
-            if(commentExists === -1){
-                throw new Error(`No comment with this id exists`);
-            }
-
-            const deletedComments = db.comments.splice(commentExists, 1);
-
-            return deletedComments[0];
-        },
-    }
-
-    export default Mutation;
+export default Mutation;
